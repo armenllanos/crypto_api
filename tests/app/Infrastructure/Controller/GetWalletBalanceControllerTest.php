97eb36d3b3eb91477aac1986d7200ad6f0bc8594
rubeniriso <rubenirisoe@gmail.com>
2022-05-02 11:53:50 +0200

    [6] - Wallet ballance controller wallet not found refactor using service test passed

--- tests/app/Infrastructure/Controller/GetWalletBalanceControllerTest.php
+++ tests/app/Infrastructure/Controller/GetWalletBalanceControllerTest.php
@@ -1,16 +1,38 @@
 <?php
 
 namespace Tests\app\Infrastructure\Controller;
+use App\Application\WalletDataSource\WalletDataSource;
+use Exception;
+use Mockery;
 use Tests\TestCase;
 
 class GetWalletBalanceControllerTest extends TestCase
 {
+    private WalletDataSource $walletDataSource;
+
+    /**
+     * @setUp
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->walletDataSource = Mockery::mock(WalletDataSource::class);
+        $this->app->bind(WalletDataSource::class, fn () => $this->walletDataSource);
+    }
     /**
      * @test
      */
     public function walletWithGivenIdDoesNotExist()
     {
+        $this->walletDataSource
+            ->expects("getWallet")
+            ->with('999')
+            ->once()
+            ->andThrow(new Exception('Wallet not found'));
+
         $response = $this->get('api/wallet/999/balance');
+
         $response->assertExactJson(['error' => 'a wallet with the specified ID was not found']);
     }
 }

9b9d220f4f0e19bc18caa5ec2821779695cc2711
armenllanos <armendariz.132110@e.unavarra.es>
2022-05-02 10:31:02 +0000

    test controllerErrorCreateWalletTest superado

--- tests/app/Infrastructure/Controller/CreateWalletControllerTest.php
+++ tests/app/Infrastructure/Controller/CreateWalletControllerTest.php
@@ -3,9 +3,11 @@
 namespace Tests\app\Infrastructure\Controller;
 
 
+
 use App\Application\WalletDataSource\WalletDataSource;
 use App\Application\WalletId\WalletIdGenerator;
 use App\Domain\Wallet;
+use Exception;
 use Illuminate\Support\Facades\Cache;
 use Mockery;
 use Tests\TestCase;
@@ -36,5 +38,18 @@ class CreateWalletControllerTest extends TestCase
             ->andReturn('1234');
         $response = $this->post('/api/wallet/open');
         $response->assertExactJson(['wallet_id' => '1234']);
+    }
+    /**
+     * @test
+     */
+    public function controllerErrorCreateWalletTest()
+    {
+        $wallet = new Wallet();
+        $wallet->setWalletId('1234');
+        $this->walletIdGenerator
+            ->expects('generateId')
+            ->andThrow(new Exception('Service unavailable'));
+        $response = $this->post('/api/wallet/open');
+            $response->assertExactJson(['error' => 'Service unavailable']);
     }
 }

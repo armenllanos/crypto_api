dc2f705c8b0dd9ca76ba00025f8cb36946f8802a
rubeniriso <rubenirisoe@gmail.com>
2022-05-02 19:15:42 +0200

    [6] - Wallet balance controller tests assert refactor

--- tests/app/Infrastructure/Controller/GetWalletBalanceControllerTest.php
+++ tests/app/Infrastructure/Controller/GetWalletBalanceControllerTest.php
@@ -4,6 +4,7 @@ namespace Tests\app\Infrastructure\Controller;
 
 use App\Application\WalletDataSource\WalletDataSource;
 use Exception;
+use Illuminate\Http\Response;
 use Mockery;
 use Tests\TestCase;
 
@@ -36,7 +37,8 @@ class GetWalletBalanceControllerTest extends TestCase
 
         $response = $this->get('api/wallet/999/balance');
 
-        $response->assertExactJson(['error' => 'a wallet with the specified ID was not found']);
+        $response->assertStatus(Response::HTTP_NOT_FOUND)
+            ->assertExactJson(['error' => 'a wallet with the specified ID was not found']);
     }
 
     /**
@@ -52,6 +54,7 @@ class GetWalletBalanceControllerTest extends TestCase
 
         $response = $this->get('api/wallet/999/balance');
 
-        $response->assertExactJson(['error' => 'service is unavailable']);
+        $response->assertStatus(Response::HTTP_SERVICE_UNAVAILABLE)
+            ->assertExactJson(['error' => 'service is unavailable']);
     }
 }

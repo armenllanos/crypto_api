47021b3e5443db2550da1d958fee690daca0ace1
David Casajus <david.casajus@gmail.com>
2022-05-02 11:14:31 +0200

    [7] - Get coin status service and test

---
+++ app/Infrastructure/Controllers/GetCoinController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Infrastructure\Controllers;
+
+use App\Application\CoinDataSource\CoinDataSource;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Response;
+use Illuminate\Routing\Controller as BaseController;
+use PHPUnit\Util\Exception;
+
+class GetCoinController extends BaseController
+{
+
+    /**
+     * @var CoinDataSource
+     */
+    private $coinStatusService;
+
+    /**
+     * @param $coinStatusService
+     */
+    public function __construct($coinStatusService)
+    {
+        $this->coinStatusService = $coinStatusService;
+    }
+
+    public function __invoke(string $id_coin): JsonResponse
+    {
+        try {
+            $coin = $this->coinStatusService->getCoinStatus($id_coin);
+
+            if(is_null($coin))
+                return response()->json(['error' => 'A coin with the specified ID was not found'], Response::HTTP_BAD_REQUEST);
+        } catch (Exception $exception) {
+            return response()->json(['error' => $exception->getMessage()], Response::HTTP_SERVICE_UNAVAILABLE);
+        }
+
+
+        return response()->json(['coin_id' => $coin->getId(), 'symbol' => $coin->getEmail(), 'name' => $coin->getEmail()
+            , 'nameid' => $coin->getEmail(), 'rank' => $coin->getEmail(), 'price_usd' => $coin->getEmail()], Response::HTTP_OK);
+
+    }
+
+}
---
+++ tests/app/Application/Coin/CoinDataSourceTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Tests\app\Application\Coin;
+
+use App\Application\CoinDataSource\CoinDataSource;
+use App\Application\CoinStatus\CoinStatusService;
+use App\Domain\Coin;
+use PHPUnit\Framework\TestCase;
+use Exception;
+use Mockery;
+
+class CoinDataSourceTest extends TestCase
+{
+    private CoinDataSource $coinDataSource;
+    private CoinStatusService $getCoinStatusService;
+
+    /**
+     * @setUp
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->coinDataSource = Mockery::mock(CoinDataSource::class);
+
+        $this->getCoinStatusService = new CoinStatusService($this->coinDataSource);
+    }
+
+
+    /**
+     * @test
+     */
+    public function getBitcoinTest()
+    {
+
+        $coinId = '90';
+
+        $coin = new Coin("id","BTC","name","nameid","price_usd",1);
+        $this->coinDataSource
+            ->expects('getCoinStatus')
+            ->with($coinId)
+            ->once()
+            ->andReturn($coin);
+
+        $response = $this->getCoinStatusService->getCoinStatus($coinId);
+
+        $this->assertEquals("BTC",$response->getSymbol());
+
+    }
+}

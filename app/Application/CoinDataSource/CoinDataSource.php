9a54058aa3260a082637ea3ba6dc5d74e850f78a
David Casajus <david.casajus@gmail.com>
2022-05-02 11:14:31 +0200

    [7] - Get coin status service and test

--- app/Application/CoinDataSource/CoinDataSource.php
+++ app/Application/CoinDataSource/CoinDataSource.php
@@ -7,6 +7,7 @@ use App\Domain\Coin;
 class CoinDataSource
 {
 
+    public function getCoinStatus(string $idCoin) : Coin;
 
     public function __construct()
     {
@@ -33,5 +34,4 @@ class CoinDataSource
         return new Coin($currency_data[0]['id'], $currency_data[0]['symbol'], $currency_data[0]['name'], $currency_data[0]['nameid'],
             $currency_data[0]['price_usd'], $currency_data[0]['rank']);
     }
-
 }
--- app/Application/CoinStatus/CoinStatusService.php
+++ app/Application/CoinStatus/CoinStatusService.php
@@ -4,7 +4,8 @@ namespace App\Application\CoinStatus;
 use App\Domain\Coin;
 use App\Application\CoinDataSource\CoinDataSource;
 
-class CoinStatusService
+
+class CoinStatusService implements CoinDataSource
 {
     /**
      * @var CoinDataSource
@@ -23,5 +24,22 @@ class CoinStatusService
     public function execute(string $idCoin) : Coin
     {
         return $this->coinDataSource->getCoinStatus($idCoin);
+    }
+    public function getCoinStatus(string $idCoin) : Coin
+    {
+        $curl = curl_init();
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => "https://api.coinlore.net/api/ticker/?id=" . $idCoin,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "GET"));
+        $response = curl_exec($curl);
+        $currency_data = json_decode($response, true);
+        return new Coin($currency_data[0]['id'], $currency_data[0]['symbol'], $currency_data[0]['name'], $currency_data[0]['nameid'],
+            $currency_data[0]['price_usd'], $currency_data[0]['rank']);
+
     }
 }
